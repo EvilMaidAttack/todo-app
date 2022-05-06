440be62d4e57f3e3e87241d2d5ebb36d8100f96f
Iraj Mashar <lrinchan.kagamine@gmail.com>
2022-05-07 01:53:52 +0200

    added taskboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorkItemComponent } from './work-item/work-item.component';
+import { TaskBoardComponent } from './task-board/task-board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WorkItemComponent
+    WorkItemComponent,
+    TaskBoardComponent
   ],
   imports: [
     BrowserModule,
